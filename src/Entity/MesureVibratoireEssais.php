261facf49a5db574236ddee22c3ea65617c00730
Aboubacar23 <aboubacarsidikiconde23@gmail.com>
2023-12-08 16:11:44 +0100

    ajout indice B suite

--- src/Entity/MesureVibratoireEssais.php
+++ src/Entity/MesureVibratoireEssais.php
@@ -34,43 +34,80 @@ class MesureVibratoireEssais
     private ?float $n25 = null;
 
     #[ORM\Column(nullable: true)]
+    private ?float $n35 = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $n45 = null;
+
+    #[ORM\Column(nullable: true)]
     private ?float $a10 = null;
 
     #[ORM\Column(nullable: true)]
     private ?float $a25 = null;
+    #[ORM\Column(nullable: true)]
+    private ?float $a35 = null;
+    #[ORM\Column(nullable: true)]
+    private ?float $a45 = null;
 
     #[ORM\Column(nullable: true)]
     private ?float $b10 = null;
 
     #[ORM\Column(nullable: true)]
     private ?float $b25 = null;
+    #[ORM\Column(nullable: true)]
+    private ?float $b35 = null;
+    #[ORM\Column(nullable: true)]
+    private ?float $b45 = null;
 
     #[ORM\Column(nullable: true)]
     private ?float $c10 = null;
 
     #[ORM\Column(nullable: true)]
     private ?float $c25 = null;
+    #[ORM\Column(nullable: true)]
+    private ?float $c35 = null;
+    #[ORM\Column(nullable: true)]
+    private ?float $c45 = null;
 
     #[ORM\Column(nullable: true)]
     private ?float $d10 = null;
 
     #[ORM\Column(nullable: true)]
     private ?float $d25 = null;
+    #[ORM\Column(nullable: true)]
+    private ?float $d35 = null;
+    #[ORM\Column(nullable: true)]
+    private ?float $d45 = null;
 
     #[ORM\Column(nullable: true)]
     private ?float $e10 = null;
 
     #[ORM\Column(nullable: true)]
     private ?float $e25 = null;
+    #[ORM\Column(nullable: true)]
+    private ?float $e45 = null;
+    #[ORM\Column(nullable: true)]
+    private ?float $e35 = null;
 
     #[ORM\Column(nullable: true)]
     private ?float $f10 = null;
 
     #[ORM\Column(nullable: true)]
     private ?float $f25 = null;
+    #[ORM\Column(nullable: true)]
+    private ?float $f35 = null;
+
+    #[ORM\Column(nullable: true)]
+    private ?float $f45 = null;
+
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $obervation = null;
+
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $titre35 = null;
+    #[ORM\Column(length: 255, nullable: true)]
+    private ?string $titre45 = null;
 
     #[ORM\Column(nullable: true)]
     private ?bool $etat = null;
@@ -154,6 +191,28 @@ class MesureVibratoireEssais
 
         return $this;
     }
+    public function getN35(): ?float
+    {
+        return $this->n35;
+    }
+
+    public function setN35(?float $n35): self
+    {
+        $this->n35 = $n35;
+
+        return $this;
+    }
+    public function getN45(): ?float
+    {
+        return $this->n45;
+    }
+
+    public function setN45(?float $n45): self
+    {
+        $this->n45 = $n45;
+
+        return $this;
+    }
 
     public function getN25(): ?float
     {
@@ -190,7 +249,29 @@ class MesureVibratoireEssais
 
         return $this;
     }
+    public function getA35(): ?float
+    {
+        return $this->a35;
+    }
 
+    public function setA35(?float $a35): self
+    {
+        $this->a35 = $a35;
+
+        return $this;
+    }
+
+    public function getA45(): ?float
+    {
+        return $this->a45;
+    }
+
+    public function setA45(?float $a45): self
+    {
+        $this->a45 = $a45;
+
+        return $this;
+    }
     public function getB10(): ?float
     {
         return $this->b10;
@@ -211,6 +292,28 @@ class MesureVibratoireEssais
     public function setB25(?float $b25): self
     {
         $this->b25 = $b25;
+
+        return $this;
+    }
+    public function getB35(): ?float
+    {
+        return $this->b35;
+    }
+
+    public function setB35(?float $b35): self
+    {
+        $this->b35 = $b35;
+
+        return $this;
+    }
+    public function getB45(): ?float
+    {
+        return $this->b45;
+    }
+
+    public function setB45(?float $b45): self
+    {
+        $this->b45 = $b45;
 
         return $this;
     }
@@ -238,6 +341,28 @@ class MesureVibratoireEssais
 
         return $this;
     }
+    public function getC35(): ?float
+    {
+        return $this->c35;
+    }
+
+    public function setC35(?float $c35): self
+    {
+        $this->c35 = $c35;
+
+        return $this;
+    }
+    public function getC45(): ?float
+    {
+        return $this->c45;
+    }
+
+    public function setC45(?float $c45): self
+    {
+        $this->c45 = $c45;
+
+        return $this;
+    }
 
     public function getD10(): ?float
     {
@@ -259,6 +384,28 @@ class MesureVibratoireEssais
     public function setD25(?float $d25): self
     {
         $this->d25 = $d25;
+
+        return $this;
+    }
+    public function getD35(): ?float
+    {
+        return $this->d35;
+    }
+
+    public function setD35(?float $d35): self
+    {
+        $this->d35 = $d35;
+
+        return $this;
+    }
+    public function getD45(): ?float
+    {
+        return $this->d45;
+    }
+
+    public function setD45(?float $d45): self
+    {
+        $this->d45 = $d45;
 
         return $this;
     }
@@ -286,6 +433,28 @@ class MesureVibratoireEssais
 
         return $this;
     }
+    public function getE35(): ?float
+    {
+        return $this->e35;
+    }
+
+    public function setE35(?float $e35): self
+    {
+        $this->e35 = $e35;
+
+        return $this;
+    }
+    public function getE45(): ?float
+    {
+        return $this->e45;
+    }
+
+    public function setE45(?float $e45): self
+    {
+        $this->e45 = $e45;
+
+        return $this;
+    }
 
     public function getF10(): ?float
     {
@@ -310,6 +479,28 @@ class MesureVibratoireEssais
 
         return $this;
     }
+    public function getF35(): ?float
+    {
+        return $this->f35;
+    }
+
+    public function setF35(?float $f35): self
+    {
+        $this->f35 = $f35;
+
+        return $this;
+    }
+    public function getF45(): ?float
+    {
+        return $this->f45;
+    }
+
+    public function setF45(?float $f45): self
+    {
+        $this->f45 = $f45;
+
+        return $this;
+    }
 
     public function getObervation(): ?string
     {
@@ -320,6 +511,27 @@ class MesureVibratoireEssais
     {
         $this->obervation = $obervation;
 
+        return $this;
+    }
+
+    public function getTitre35(): ?string
+    {
+        return $this->titre35;
+    }
+
+    public function setTitre35(?string $titre35): self
+    {
+        $this->titre35 = $titre35;
+        return $this;
+    }
+    public function getTitre45(): ?string
+    {
+        return $this->titre45;
+    }
+
+    public function setTitre45(?string $titre45): self
+    {
+        $this->titre45 = $titre45;
         return $this;
     }
     
