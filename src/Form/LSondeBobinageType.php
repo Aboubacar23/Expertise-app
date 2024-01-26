632d7d0f0593ad904d8346c77a2a84a15aab7f97
Aboubacar23 <aboubacarsidikiconde23@gmail.com>
2024-01-26 08:29:21 +0100

    Version 1.0.0

--- src/Form/LSondeBobinageType.php
+++ src/Form/LSondeBobinageType.php
@@ -48,6 +48,10 @@ class LSondeBobinageType extends AbstractType
                     'Rotor' => 'Rotor',
                     "Stator 2" => "Stator 2",
                     "Rotor 2" => "Rotor 2",
+                    "Stator AE" => "Stator AE",
+                    'Rotor AE' => 'Rotor AE',
+                    "Régulation" => "Régulation",
+                    "Jeu de Bague" => "Jeu de Bague",
                     "Sondes" => "Sondes",
                     "Rechauffage" => "Rechauffage",
                 ]
